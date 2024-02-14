9ab5ce03989892148f3a68d129a2206e2f72580d
Fabrizio <fabriziofontana02@gmail.com>
2024-02-14 08:29:58 +0100

    fixed residuals plot

--- airquality.r
+++ airquality.r
@@ -2,14 +2,32 @@ data("airquality")
 ?airquality
 data <- data.frame(airquality)
 
+Ozono <- data$Ozone
+Temperatura <- data$Temp
+
+
 coeff_corr <- cor(data$Temp, data$Ozone, use = "complete.obs")
 
-mod <- lm(Ozone ~ Temp, data = data)
+mod <- lm(Ozono ~ Temperatura, data = data)
 
 summary(mod)
 
-plot(data$Ozone ~ data$Temp)
+plot(Ozono ~ Temperatura, 
+     main = "Scatter plot: Livello di Ozono vs. Temperatura", 
+     xlab = "Temperatura [°F]", 
+     ylab = "Livello di Ozono [ppm]")
 
-abline(mod, col = "black", lwd = 2)
+abline(mod, col = "red", lwd = 2)
 
 print(shapiro.test(resid(mod)))
+
+residui <- resid(mod)
+
+residui_standardizzati <- resid(mod) / sd(resid(mod))
+
+plot(fitted(mod), residui_standardizzati, 
+     main = "Grafico dei residui", 
+     xlab = "Valori del predittore X", 
+     ylab = "Residui standardizzati")
+
+abline(h = 0, col = "red")
